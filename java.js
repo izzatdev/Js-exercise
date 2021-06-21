65475796ed990a62a84605248ef81516621e8d7c
izzatdev <toshpulatovizzatulla@gmail.com>
2021-06-21 22:28:53 +0500

    input number.output day of week which stays on this index

---
+++ java.js
@@ -0,0 +1,82 @@
+// //1-masala
+// var a=Math.round(Math.random()*100)
+// console.log(a)
+// if(a%2==0)
+// {
+//     console.log("Juft")
+// }
+// else
+// {
+//     console.log("toq")
+// }
+//
+//
+// //2-masalalar to'plami:
+// //1-masala.
+// var a=Math.round(Math.random()*100)
+// console.log(a)
+// if(a%5==0)
+// {
+//     console.log("Beshga bo'linadi")
+// }
+// else
+// {
+//     console.log("Beshga bo'linmaydi")
+// }
+//
+// //2-masala.
+// var a=Math.round(Math.random()*100)
+// console.log(a)
+// if(a%3==0)
+// {
+//     console.log("Uchga bo'linadi")
+// }
+// else
+// {
+//     console.log("Uchga bo'linmaydi")
+// }
+//
+// //3-masala.
+// var a=Math.round(Math.random()*100)
+// console.log(a)
+// if(a%3==0&&a%5==0)
+// {
+//     console.log("Uchga va Beshga bo'linadi")
+// }
+// else
+// {
+//     console.log("Uchga va Beshga bo'linmaydi")
+// }
+//
+// //4-masala
+// var a=Math.round(Math.random()*100);
+// var b=Math.round(Math.random()*100);
+// console.log("a=",a)
+// console.log("b=",b)
+//
+// if(Math.abs(a-5)<Math.abs(b-5))
+// {
+//     console.log("a soni yaqin")
+// }
+// else
+// {
+//     console.log("b soni yaqin")
+// }
+
+// let a;
+// a=prompt();
+// a=Math.round(a);
+// switch (a)
+// {
+//     case 1: console.log("Dushanba");break;
+//     case 2: console.log("Seshanba");break;
+//     case 3: console.log("Chorshanba");break;
+//     case 4: console.log("Payshanba");break;
+//     case 5: console.log("Juma");break;
+//     case 6: console.log("Shanba");break;
+//     case 7: console.log("Yakshanba");break;
+//     default: console.log("Bunday hafta kuni yoq");break;
+// }
+
+
+
